7d7c10c91d8698c6973c503fe4f68278513d192e
touchkop99 <73217389+touchkop99@users.noreply.github.com>
2021-10-05 11:53:28 +0700

    Auto-generated commit [ci skip]

---
+++ 9072.b4336461e671dd22453d.js
@@ -0,0 +1,1 @@
+(self.webpackChunktest_app=self.webpackChunktest_app||[]).push([[9072],{9072:(s,t,o)=>{"use strict";o.r(t),o.d(t,{ion_text:()=>l});var r=o(3150),c=o(7585),e=o(1269);const l=class{constructor(s){(0,r.r)(this,s)}render(){const s=(0,c.b)(this);return(0,r.h)(r.H,{class:(0,e.c)(this.color,{[s]:!0})},(0,r.h)("slot",null))}};l.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
